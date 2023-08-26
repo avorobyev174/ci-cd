0af4d1994a7f91a583d30a7eedbb49853c0328aa
Alexey <avorobyev174@gmail.com>
2023-08-26 20:29:59 +0500

    change counter

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
 
     return (
         <div className="App">
-            <h1 style={{ fontSize: 70, color: 'red' }}> value = { state } </h1>
+            <h1 style={{ fontSize: 70, color: 'black' }}> value = { state } </h1>
             <button
-                style={{ background: 'black', color: "white", padding: 15 }}
+                style={{ background: 'green', color: "black", padding: 15 }}
                 onClick={ increment }
             >
                 increment
